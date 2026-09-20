f13c9b3ccb7c047b48ad67d769f1f648aa0604f2
theuniquefersa <holafersa@gmail.com>
2026-09-19 22:14:57 -0600

    feat(game): adds functionality to base Agent: hit or miss, continye

--- dynamics/script/game.js
+++ dynamics/script/game.js
@@ -13,11 +13,36 @@ compass_points.set("R", [0, 1]);
 compass_points.set("D", [1,0]);
 compass_points.set("L", [0, -1]);
 
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 class Battleship_Agent {
+    #N = 100;
     constructor(x = 10, y = 10){
         this.grid_x_size = x;
         this.grid_y_size = y;
+
         this.grid;
+        this.avail_moves = []; // with the form of a coord: A10
+        this.map_grid_to_coord = new Map();
+        this.map_coord_to_grid = new Map();
+        this.fleet = [2, 3, 3, 4, 5];
+        this.fleet_symbols = ['A', 'B', 'S', 'C', 'P'];
+        this.map_fleet = new Map([
+            ['A', 0],
+            ['B', 0],
+            ['S', 0],
+            ['C', 0],
+            ['P', 0]
+        ]);
+        this.map_fleet_terminal = new Map([
+            ['A', 5],
+            ['B', 4],
+            ['S', 3],
+            ['C', 3],
+            ['P', 2]
+        ]);
         this.#init_grid(x, y);
         this.#init_grid_map(x, y);
         this.#generate_fleet_locations();
@@ -39,9 +64,19 @@ class Battleship_Agent {
                 let codigo_actual = vertical_axis.charCodeAt(0);
                 let new_corrd_x = codigo_actual + i;
                 let X = String.fromCharCode(new_corrd_x);
-                console.log(`${X + Y}`)
+                //console.log(`${X + Y}`)
                 //this.grid[i][j-1] = X + Y;
+                let string_key = X + Y;
+                let tuple_numbers = `${i},${j-1}`;
+                this.avail_moves.push(string_key);
+                this.map_grid_to_coord.set(tuple_numbers, string_key);
+                this.map_coord_to_grid.set(string_key, [i, j-1]);
             }
+        }
+    }
+    print_map_to_coord(){
+        for(const [key, value] of this.map_grid_to_coord.entries()){
+            console.log(`${key}: ${value}`);
         }
     }
     draw_ship(init, size, direction, symbol) {
@@ -84,7 +119,10 @@ class Battleship_Agent {
     }
     //to override
     hunt(){
-
+        //super naive implementation
+        let size_of_availables = this.grid_x_size*this.grid_y_size;
+        let m = getRandom(0, size_of_availables);
+        return this.avail_moves[m];
     }
     target(){
 
@@ -92,22 +130,66 @@ class Battleship_Agent {
     recalc(){
 
     }
-}
-let prueba = new Battleship_Agent();
-//console.log(prueba.grid);
-prueba.print_grid()
+    //for all functoins, m has the form of a coord
+    is_hit_or_miss(m){
+        let grid = this.map_coord_to_grid.get(m);
+        let [x, y] = grid;
+        //console.log(x, y);
+        console.log(this.grid[x][y])
+        let pixel_symbol = this.grid[x][y];
+        return this.fleet_symbols.includes(pixel_symbol); //0 if is a miss, 1 if is a hit
+    }
+    hit(m){
+        let [x, y] = this.map_coord_to_grid.get(m);
+        this.grid[x][y] = 'X';
 
+        //take out the coord to avoid looping (SRA definition)
+        let index = this.avail_moves.indexOf(m);
+        if(index !== -1){
+            this.avail_moves.splice(index, 1);
+        }
+        this.#N --;
+
+        //tests:
+        console.log(this.getN());
+        this.print_grid();
+        this.printAvailableMoves();
+    }
+    miss(m){
+
+    }
+    continue(){
+        let next_target = this.hunt();
+        console.log(next_target);
+        //console.log(this.is_hit_or_miss(next_target));
+        if(this.is_hit_or_miss(next_target)){
+            this.hit(next_target);
+        } else {
+            this.miss(next_target);
+        }
+    }
+    getN(){
+        return this.#N;
+    }
+    printAvailableMoves(){
+        console.log(this.avail_moves)
+    }
+}
 
 class Battleship_SRA extends Battleship_Agent{
     //Simple Reflex Agent
 
     //modify to support NxM size of grid
     constructor(name = "Simple Reflex Agent"){
-        this.name = name;
         super()
+        this.name = name;
+        
     }
     hunt(){
-        //to it random
+        let size_of_availables = this.avail_moves.length;
+        let m = getRandom(0, size_of_availables);
+        console.log(m);
+        return this.avail_moves[m];
     }
     target(){
         //stays empty
@@ -115,15 +197,23 @@ class Battleship_SRA extends Battleship_Agent{
     recalc(){
         //stays empty
     }
+    
 }
+let prueba = new Battleship_SRA();
+//console.log(prueba.grid);
+prueba.print_grid()
+console.log(prueba.avail_moves);
+console.log(prueba.map_coord_to_grid);
+//prueba.print_map_to_coord();
+prueba.continue();
 
 class Battleship_GBA extends Battleship_Agent{
     //Goal Base Agent
 
     //modify to support NxM size of grid
     constructor(name = "Simple Reflex Agent"){
-        this.name = name;
         super()
+        this.name = name;
     }
     hunt(){
         //random according to parity
@@ -141,8 +231,8 @@ class Battleship_ABAOP extends Battleship_Agent{
 
     //modify to support NxM size of grid
     constructor(name = "Simple Reflex Agent"){
-        this.name = name;
         super()
+        this.name = name;
     }
     hunt(){
         //prob function: top most
@@ -154,3 +244,27 @@ class Battleship_ABAOP extends Battleship_Agent{
         //recalc all board: new probability function
     }
 }
+let game_status = "INIT";
+function game_continues(){
+    if(game_status != "END")
+        return 1;
+    else return 0;
+}
+function toogle_turn(){
+    if (turn) return 0;
+    else return 1;
+}
+function game(){
+    const PlayerA = new Battleship_SRA;
+    const PlayerB = new Battleship_GBA;
+    while(game_continues()){
+        if(turn){ // 1: Player B
+            PlayerB.continue();
+        }
+        else {  // 0: Player A
+            PlayerA.continue();
+        }
+        turn = toogle_turn();
+    }
+}
+//game();
